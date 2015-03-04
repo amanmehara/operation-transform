651d78cd35c79667a4cc9261d8b4516667fa7c85
amanmehara <amanmehara@hotmail.com>
2015-03-04 10:43:41 +0530

    Fixed Some Errors

--- Operation Transform/CPrograms/FirstComeFirstServeScheduling.xaml.cs
+++ Operation Transform/CPrograms/FirstComeFirstServeScheduling.xaml.cs
@@ -23,12 +23,12 @@ namespace Operation_Transform.CPrograms
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class FirstComeFirstScheduling : Page
+    public sealed partial class FirstComeFirstServeScheduling : Page
     {
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
-        public FirstComeFirstScheduling()
+        public FirstComeFirstServeScheduling()
         {
             this.InitializeComponent();
 
